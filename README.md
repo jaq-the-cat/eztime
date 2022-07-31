c094329813e776ba6b8e112fceab71dfa3173413
Jaquie <jaq.cat@protonmail.ch>
2022-07-31 12:49:52 -0300

    done ig

--- README.md
+++ README.md
@@ -1,39 +1,39 @@
-<!-- 
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+Easy to use, tiny Time library for when you just need time without all that DateTime nonsense.
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+- Conversion to and from `String`s
+- Conversion to and from milliseconds as `int`s
+- Addition and subtraction
 
 ## Getting started
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+To get started, simply import the library with `import 'package:eztime/eztime.dart'`
+and create a `Time` object
+```dart
+var t = Time.fromString("12:00:00");
+// or
+var t = Time.fromMilliseconds(43200000); // 12 hours
+// or
+var t = Time(12, 00, 00) 
+``` 
 
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder. 
-
+Addition and subtraction
 ```dart
-const like = 'sample';
+Time(12) + Time(0, 30); // 12:30:00
+Time(10) - Time(5); // 05:00:00
+Time(10, 0, 0, true) + Time(5); // -10:00:00 + 05:00:00 = -05:00:00
+```
+
+Conversion to more familiar formats
+```dart
+print(Time.fromString("00:30:00")); // 00:30:00
+print(Time(0, 1, 5).asMilliseconds); // 65000
 ```
 
 ## Additional information
 
-TODO: Tell users more about the package: where to find more information, how to 
-contribute to the package, how to file issues, what response they can expect 
-from the package authors, and more.
+Source code found at https://github.com/jaq-the-cat/eztime. Contribute at any time with anything,
+I'm pretty much always on GitHub :P
--- lib/src/eztime_base.dart
+++ lib/src/eztime_base.dart
@@ -1,3 +1,12 @@
+double _msToHours(int ms) => ((ms / 60) / 60) / 1000;
+String _intAsTime(int n) {
+  return n.toString().padLeft(2, '0');
+}
+
+String _doubleAsTime(double n) {
+  return n.toStringAsFixed(3).padLeft(6, '0');
+}
+
 /// Limiter for use in fields with a maximum value
 /// As an example, if you plug in an [n] of 25 and a [max] of 24, you will get
 /// an [extra] of 1, 1 day, and a [remaining] of 1, 1 hour
@@ -19,26 +28,17 @@ class Limit {
   }
 }
 
-double _msToHours(int ms) => ((ms / 60) / 60) / 1000;
-String _intAsTime(int n) {
-  return n.toString().padLeft(2, '0');
-}
-
-String _doubleAsTime(double n) {
-  return n.toStringAsFixed(3).padLeft(6, '0');
-}
-
 /// Represents a Time in hours, minutes and seconds. Automatically wraps values, so both
 /// Time(0, 60, 60) and Time(24, 60, 60) will return 01:01:00.
 ///
 /// This class supports addition, subtraction and conversion to and from strings and milliseconds,
 /// aswell as addition with days.
 class Time {
-  final bool negative;
+  final bool isNegative;
   int _h = 0, _m = 0;
   double _s = 0;
 
-  Time(int h, [int m = 0, double s = 0, this.negative = false]) {
+  Time(int h, [int m = 0, double s = 0, this.isNegative = false]) {
     final os = Limit(s.abs(), 60);
     final om = Limit(m.abs() + os._extra, 60);
     final oh = Limit(h.abs() + om.extra, 24);
@@ -57,33 +57,22 @@ class Time {
   Time get asNegative => Time(_h, _m, _s, true);
 
   /// Returns the sum of 2 times, with the number of days passed ommited.
-  /// For that, look at [Time.addWithDays]
   Time operator +(Time other) {
-    if (other.negative) {
+    if (other.isNegative) {
       return this - other;
-    } else if (negative) {
+    } else if (isNegative) {
       return other - this;
     }
-    return Time(_h + other._h, _m + other._m, _s + other._s, negative);
+    return Time(_h + other._h, _m + other._m, _s + other._s, isNegative);
   }
 
   /// Returns the subtraction of 2 times.
   Time operator -(Time other) {
-    int thisMs = negative ? -asMilliseconds : asMilliseconds;
+    int thisMs = isNegative ? -asMilliseconds : asMilliseconds;
     final ms = thisMs - other.asMilliseconds;
     final t = Time.fromMilliseconds(ms.abs())!;
     if (ms < 0) return t.asNegative;
     return t;
-  }
-
-  /// Adds two times and returns a list of [days, hours, minutes, seconds]
-  /// If you want the addition without the number of days returned as a [Time] object,
-  /// use the [+] operator
-  List<num> addWithDays(Time other) {
-    final s = Limit(_s + other._s, 60);
-    final m = Limit(_m + other._m + s._extra, 60);
-    final h = Limit(_h + other._h + m.extra, 24);
-    return [h._extra, h._remaining.round(), m.remaining.round(), s._remaining];
   }
 
   /// Converts a string in the format HH:MM:SS to a [Time] object. If parsing fails,
@@ -114,5 +103,5 @@ class Time {
 
   @override
   String toString() =>
-      "${negative ? "-" : ""}${_intAsTime(_h)}:${_intAsTime(_m)}:${_doubleAsTime(_s)}";
+      "${isNegative ? "-" : ""}${_intAsTime(_h)}:${_intAsTime(_m)}:${_doubleAsTime(_s)}";
 }
